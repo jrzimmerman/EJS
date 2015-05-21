48d2e85fc4e1e7d6b97d820c911947de7fbd4580
Justin Zimmerman <zimmy130@gmail.com>
2015-05-20 21:13:38 -0400

    Add my chessboard implementation

--- Chapter_2/ChessBoard.js
+++ Chapter_2/ChessBoard.js
@@ -1,6 +1,22 @@
 // Chess Board
 // My Solution
-
+function board(size) {
+  var chessboard = "";
+  var sum = 0;
+  for(var i = 1; i <= size; i++) {
+    var row = "";
+    for(var j = 1; j <= size; j++) {
+      sum = i + j;
+      if(sum % 2 === 0) {
+        row += "#";
+      } else {
+        row += " ";
+      }
+    }
+    console.log(row + "\n");
+  }
+}
+board(10);
 
 // Book Solution
 

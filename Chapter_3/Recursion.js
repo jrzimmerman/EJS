39b78ac8836c112e224a7079ca06c787f9faf845
Justin Zimmerman <zimmy130@gmail.com>
2015-05-24 11:43:53 -0400

    Recursion Solution

--- Chapter_3/Recursion.js
+++ Chapter_3/Recursion.js
@@ -1,1 +1,27 @@
 // Recursion
+// My Solution
+
+function isEven(n) {
+	if (n === 0)
+		return true;
+	else if (n === 1)
+		return false;
+	else if (n < 0)
+		return isEven(-n);
+	else
+		return isEven(n - 2);
+}
+isEven(75);
+
+// Book Solution
+
+function isEven(n) {
+  if (n == 0)
+    return true;
+  else if (n == 1)
+    return false;
+  else if (n < 0)
+    return isEven(-n);
+  else
+    return isEven(n - 2);
+}

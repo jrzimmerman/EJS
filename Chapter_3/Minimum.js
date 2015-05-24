39817278b2b44d6a81981152c89fd416d7efbfa5
Justin Zimmerman <zimmy130@gmail.com>
2015-05-24 11:28:44 -0400

    Minimum Solution

--- Chapter_3/Minimum.js
+++ Chapter_3/Minimum.js
@@ -1,1 +1,21 @@
 // Minimum
+// My Solution
+
+function min(n1, n2) {
+	if(n1 < n2 ) {
+		return n1;
+	} else {
+		return n2;
+	}
+}
+
+min(0,-10);
+
+// Book Solution
+
+function min(a, b) {
+  if (a < b)
+    return a;
+  else
+    return b;
+}

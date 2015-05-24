2583b6f0e06e9a6288dd1e2bad9985e7f85ee5f8
Justin Zimmerman <zimmy130@gmail.com>
2015-05-24 12:18:23 -0400

    Bean Counting Solution

--- Chapter_3/BeanCounting.js
+++ Chapter_3/BeanCounting.js
@@ -1,1 +1,36 @@
 // Bean Counting
+// My Solution
+
+function countBs(str) {
+	var count = 0;
+	for(var i = 0; i < str.length; i++){
+		 if (str[i].charAt() === "B")
+		 	count++;
+	}
+	return count;
+}
+countBs("BBCK");
+
+function countChar(str,char) {
+	var count = 0;
+	for(var i = 0; i < str.length; i++) {
+		if (str[i].charAt() === char)
+			count++;
+	}
+	return count;
+}
+countChar("kakkerlak", "k");
+
+// Book Solution
+
+function countChar(string, ch) {
+  var counted = 0;
+  for (var i = 0; i < string.length; i++)
+    if (string.charAt(i) == ch)
+      counted += 1;
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}

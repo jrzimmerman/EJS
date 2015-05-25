98fb2e38fe6e609a93366b8b55e01610b2d89460
Justin Zimmerman <zimmy130@gmail.com>
2015-05-25 19:16:13 -0400

    Deep Comparison Solution

--- Chapter_4/DeepComparison.js
+++ Chapter_4/DeepComparison.js
@@ -2,13 +2,51 @@
 // My Solution
 
 function deepEqual(a, b) {
-	
-	if (typeof a === typeof b)
+	if (a === b)
 		return true;
-	else if (a === b)
-		return true;
-	else
+	else if ( a === null || typeof a != "object" || 
+			  b === null || typeof b != "object")
 		return false;
+	var propA = 0, propB = 0;
+
+	for ( var prop in a ) {
+		propA += 1;
+	}
+
+	for ( var prop in b) {
+		propB += 1;
+
+		if( !(prop in a) || !deepEqual(a[prop],b[prop]))
+			return false;
+	}
+	
+	return propA == propB;
 }
 
+var obj = {here: {is: "an"}, object: 2};
+// deepEqual(obj, obj);
+// deepEqual(obj, {here: 1, object: 2});
+deepEqual(obj, {here: {is: "an"}, object: 2});
+
 // Book Solution
+
+function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object")
+    return false;
+  
+  var propsInA = 0, propsInB = 0;
+
+  for (var prop in a)
+    propsInA += 1;
+
+  for (var prop in b) {
+    propsInB += 1;
+    if (!(prop in a) || !deepEqual(a[prop], b[prop]))
+      return false;
+  }
+
+  return propsInA == propsInB;
+}

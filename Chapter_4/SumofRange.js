57bc24d011ff935b327b78997dedca3dfe9e882f
Justin Zimmerman <zimmy130@gmail.com>
2015-06-14 09:05:29 -0400

    Sum of Range Solution

--- Chapter_4/SumofRange.js
+++ Chapter_4/SumofRange.js
@@ -1,32 +1,32 @@
 // Sum of a Range
 // My Solution
-function range(start, end, step) {
-	if (step === null) {
-	    step = 1;
-	}
-	var array = [];	
-	if (step > 0) {
-		for (var i = start; i <= end; i += step) {
-			array.push(i);
+function range(start,end,step){
+	if(step === undefined){
+		    step = 1;
 		}
+	var arr = [];
+	if(start<end) {
+    	for (start; start <= end; start += step) {
+    		arr.push(start);
+    	}
 	} else {
-		for (var i = start; i >= end; i += step) {
-			array.push(i);
-		}
+	    for(start;start >= end; start += step) {
+	        arr.push(start);
+	    }
 	}
-	return array;
+	
+	return arr;
 }
 
-function sum(array) {
-    var result = 0;
-	for(var i = 0; i < array.length; i++){
-		result += array[i];
-	}
-	return result;
+function sum(arr) {
+    var total = 0;
+    for(var i = 0; i < arr.length;i++) {
+        total += arr[i];
+    }
+    return total;
 }
 
-sum(range(1,10,1));
-//55
+sum(range(1,10));
 range(10,1,-1);
 
 // Book Solution
